1e9e8d39f10faeeb846e38227abe90e07c485f95
petar <petaracimovic990@gmail.com>
2023-11-17 20:05:42 +0100

    Making user stats basic template

--- Frontend/src/app/components/user-stats/user-stats.component.ts
+++ Frontend/src/app/components/user-stats/user-stats.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class UserStatsComponent {
 
+  user_id: string = ''
+  date: string = ''
+
+
+  getUserStats(): void {
+    console.log("User id: " + this.user_id + ", Date: " + this.date)
+  }
+
+
+  isDateFormatValid(): boolean {
+    // Regular expression for the "YYYY-MM-DD" format
+    var dateFormat = /^\d{4}-\d{2}-\d{2}$/;
+    // Test if the date string matches the format
+    return dateFormat.test(this.date);
+}
 }
