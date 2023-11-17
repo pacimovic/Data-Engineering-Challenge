92b7e1379dd20d39f24595f0d3600f6ec3d644c6
petar <petaracimovic990@gmail.com>
2023-11-17 18:11:18 +0100

    Added user and game stats components

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserStatsComponent } from './components/user-stats/user-stats.component';
+import { GameStatsComponent } from './components/game-stats/game-stats.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "user-stats",
+    component: UserStatsComponent
+  },
+  {
+    path: "game-stats",
+    component: GameStatsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app/app.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from "@angular/common/http";
+import { UserStatsComponent } from './components/user-stats/user-stats.component';
+import { GameStatsComponent } from './components/game-stats/game-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserStatsComponent,
+    GameStatsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
